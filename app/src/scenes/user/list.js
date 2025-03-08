34c3decc88a4e70c5e0ca6574dbb0903d42b43ca
Théo TURPIN <theo.turpin@epitech.eu>
2025-03-08 16:21:06 +0100

    feat: feat KEY Features hide password

--- app/src/scenes/user/list.js
+++ app/src/scenes/user/list.js
@@ -87,79 +87,98 @@ const NewList = () => {
 
 const Create = () => {
   const [open, setOpen] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const history = useHistory();
 
   return (
-    <div style={{ marginBottom: 10 }}>
-      <div className="text-right">
-        <button className="bg-[#0560FD] text-[#fff] py-[12px] px-[22px] w-[170px] h-[48px]	rounded-[10px] text-[16px] font-medium" onClick={() => setOpen(true)}>
-          Create new user
-        </button>
-      </div>
-      {open ? (
-        <div className=" absolute top-0 bottom-0 left-0 right-0  bg-[#00000066] flex justify-center p-[1rem] z-50 " onClick={() => setOpen(false)}>
-          <div
-            className="w-full md:w-[60%] h-fit  bg-[white] p-[25px] rounded-md"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}>
-            <Formik
-              initialValues={{}}
-              onSubmit={async (values, { setSubmitting }) => {
-                try {
-                  values.status = "active";
-                  values.availability = "not available";
-                  values.role = "ADMIN";
-                  const res = await api.post("/user", values);
-                  if (!res.ok) throw res;
-                  toast.success("Created!");
-                  setOpen(false);
-                  history.push(`/user/${res.data._id}`);
-                } catch (e) {
-                  console.log(e);
-                  toast.error("Some Error!", e.code);
-                }
-                setSubmitting(false);
-              }}>
-              {({ values, handleChange, handleSubmit, isSubmitting }) => (
-                <React.Fragment>
-                  <div>
-                    <div className="flex justify-between flex-wrap">
-                      <div className="w-full md:w-[48%] mt-2">
-                        <div className="text-[14px] text-[#212325] font-medium	">Name</div>
-                        <input className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]" name="name" value={values.name} onChange={handleChange} />
-                      </div>
-                      <div className="w-full md:w-[48%] mt-2">
-                        <div className="text-[14px] text-[#212325] font-medium	">Email</div>
-                        <input className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]" name="email" value={values.email} onChange={handleChange} />
-                      </div>
-                    </div>
-                    <div className="flex justify-between flex-wrap mt-3">
-                      {/* Password */}
-                      <div className="w-full md:w-[48%] mt-2">
-                        <div className="text-[14px] text-[#212325] font-medium	">Password</div>
-                        <input className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]" name="password" value={values.password} onChange={handleChange} type={"password"}/>
-                      </div>
-                    </div>
-                  </div>
-
-                  <br />
-                  <LoadingButton
-                    className="mt-[1rem]  bg-[#0560FD] text-[16px] font-medium text-[#FFFFFF] py-[12px] px-[22px] rounded-[10px]"
-                    loading={isSubmitting}
-                    onClick={handleSubmit}>
-                    Save
-                  </LoadingButton>
-                </React.Fragment>
-              )}
-            </Formik>
-          </div>
+      <div style={{ marginBottom: 10 }}>
+        <div className="text-right">
+          <button className="bg-[#0560FD] text-[#fff] py-[12px] px-[22px] w-[170px] h-[48px] rounded-[10px] text-[16px] font-medium" onClick={() => setOpen(true)}>
+            Create new user
+          </button>
         </div>
-      ) : null}
-    </div>
+        {open ? (
+            <div className="absolute top-0 bottom-0 left-0 right-0 bg-[#00000066] flex justify-center p-[1rem] z-50" onClick={() => setOpen(false)}>
+              <div
+                  className="w-full md:w-[60%] h-fit bg-[white] p-[25px] rounded-md"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+              >
+                <Formik
+                    initialValues={{}}
+                    onSubmit={async (values, { setSubmitting }) => {
+                      try {
+                        values.status = "active";
+                        values.availability = "not available";
+                        values.role = "ADMIN";
+                        const res = await api.post("/user", values);
+                        if (!res.ok) throw res;
+                        toast.success("Created!");
+                        setOpen(false);
+                        history.push(`/user/${res.data._id}`);
+                      } catch (e) {
+                        console.log(e);
+                        toast.error("Some Error!", e.code);
+                      }
+                      setSubmitting(false);
+                    }}
+                >
+                  {({ values, handleChange, handleSubmit, isSubmitting }) => (
+                      <React.Fragment>
+                        <div>
+                          <div className="flex justify-between flex-wrap">
+                            <div className="w-full md:w-[48%] mt-2">
+                              <div className="text-[14px] text-[#212325] font-medium">Name</div>
+                              <input className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]" name="name" value={values.name} onChange={handleChange} />
+                            </div>
+                            <div className="w-full md:w-[48%] mt-2">
+                              <div className="text-[14px] text-[#212325] font-medium">Email</div>
+                              <input className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]" name="email" value={values.email} onChange={handleChange} />
+                            </div>
+                          </div>
+                          <div className="flex justify-between flex-wrap mt-3">
+                            {/* Password */}
+                            <div className="relative w-full md:w-[48%] mt-2">
+                              <div className="text-[14px] text-[#212325] font-medium">Password</div>
+                              <div className="flex items-center border border-[#FFFFFF] rounded-[10px]">
+                                <input
+                                    className="projectsInput text-[14px] font-normal text-[#212325] rounded-l-[10px] w-full"
+                                    name="password"
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    type={showPassword ? 'text' : 'password'}
+                                />
+                                <button
+                                    className="bg-[#F9FBFD] p-2 rounded-r-[10px] border-l border-[#FFFFFF]"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                >
+                                  {showPassword ? "👀" : "🙈"}
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <br />
+                        <LoadingButton
+                            className="mt-[1rem] bg-[#0560FD] text-[16px] font-medium text-[#FFFFFF] py-[12px] px-[22px] rounded-[10px]"
+                            loading={isSubmitting}
+                            onClick={handleSubmit}
+                        >
+                          Save
+                        </LoadingButton>
+                      </React.Fragment>
+                  )}
+                </Formik>
+              </div>
+            </div>
+        ) : null}
+      </div>
   );
 };
+
 
 const SelectAvailability = ({ filter, setFilter }) => {
   return (
